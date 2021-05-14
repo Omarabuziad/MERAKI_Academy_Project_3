024212223c75f405c15f9f7ac0ef0a90e7cac076
user name <email@domain.com>
2021-05-14 20:58:59 +0400

    getAllArticles

--- main.js
+++ main.js
@@ -1,9 +1,63 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 5000 ;
 
 // a middleware that enables us to read the received JSON data
 app.use(express.json());
+
+
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+];
+
+
+app.get("/articles" , (req , res , next )=> {
+    res.status(200);
+    res.json(articles)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
